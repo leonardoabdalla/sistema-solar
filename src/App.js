98ce96051a234c50118d684519bdd5a6c39abad3
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-01-25 17:10:04 -0300

    requisito 02 e 03

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
-    return (<Header> </Header>);
+    return (
+      <div>
+        <Header> </Header>
+        <SolarSystem> </SolarSystem>
+        <Title> </Title>
+      </div>);
   }
 }
 
