29f1e6737487fbbcfae594432d1720365e4aadbe
Leonardo Abdalla <leonardoabdalladeveloper@gmail.com>
2022-01-26 10:14:37 -0300

    requisito 09

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SolarSystem from './components/SolarSystem';
 import Title from './components/Title';
 import PlanetCard from './components/PlanetCard';
 import Missions from './components/Missions';
+import MissionCard from './components/MissionCard';
 
 class App extends React.Component {
   render() {
@@ -14,6 +15,7 @@ class App extends React.Component {
         <Missions> </Missions>
         <Title> </Title>
         <PlanetCard> </PlanetCard>
+        <MissionCard> </MissionCard>
       </div>);
   }
 }
